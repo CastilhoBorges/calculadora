9198c65e13f28ba60efef4304159059a9b9ea385
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-07-12 11:18:00 -0300

    .

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-function appendToDisplay(value) {
-    document.querySelector('#display').value += value ;
-}
+"use strict";
 
-function clearDisplay() {
-    document.querySelector('#display').value = '';
-}
+/*
 
-function calculateResult() {
-    try {
-        const result = eval(document.querySelector('#display').value);
-        document.querySelector('#display').value = result
-    } catch (error) {
-        document.querySelector('#display').value = 'Erro'
-    }
-}
+1 - Definir todas as variaveis que usaremos
+2 - Fazer com que quando clicarmos um um numero ele projete no input 
+3 - Quando clicarmos em um botão de operador, ele tem q fazer a soma com o proximo numeoro que digitarmos 
+4 - Quando clicarmos no igual ele faz a conta 
+5 - fazer com que o botão de limpeza limpe tudo 
+
+ */
+
+const appendToDisplay = function (value) {
+  document.querySelector("#display").value += value;
+};
+
+const clearDisplay = function () {
+  document.querySelector("#display").value = "";
+};
+
+const calculateResult = function () {
+    
+};
